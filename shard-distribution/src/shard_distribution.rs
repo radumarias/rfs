5c1f316f653a3390a3969947a397bbc9e9714d61
Radu Marias <radumarias@gmail.com>
2024-08-02 00:10:45 +0300

    Fix tests

--- shard-distribution/src/shard_distribution.rs
+++ shard-distribution/src/shard_distribution.rs
@@ -23,7 +23,7 @@ pub trait ShardDistribution {
     ///
     /// Returns a list of nodes based on entity replica count.
     /// If there are not enough nodes, it returns the max-available nodes.
-    fn get_nodes(
+    fn distribute(
         &mut self,
         entity: String,
         entity_replicas: Option<u16>,
@@ -47,42 +47,50 @@ mod tests {
             ("node4".to_string(), 35_f64),
             ("node5".to_string(), 42_f64),
         ]
-            .into_iter()
-            .collect();
+        .into_iter()
+        .collect();
         let mut hasher = ConsistentHashing::new(nodes, 5);
-        let nodes = hasher.get_nodes("key1".to_string(), Some(3), Some(10_f64));
+        let nodes = hasher.distribute("key1".to_string(), Some(3), Some(10_f64));
         let mut nodes = nodes.iter().cloned().collect::<Vec<String>>();
         nodes.sort_unstable();
+        println!("key1 {nodes:?} \n");
         assert_eq!(nodes.len(), 3);
 
         let node = nodes.last().unwrap().clone();
         hasher.remove(node.clone());
-        let nodes = hasher.get_nodes("key1".to_string(), Some(3), None);
+        let nodes = hasher.distribute("key1".to_string(), Some(3), None);
+        println!("{node} removed key1 {nodes:?} \n");
         assert!(!nodes.contains(&node));
 
-        let nodes = hasher.get_nodes("key1".to_string(), None, None);
+        let nodes = hasher.distribute("key2".to_string(), None, None);
+        println!("key2 no replicas {nodes:?} \n");
         assert!(nodes.is_empty());
 
         hasher.add("node6".to_string(), 10_f64);
-        let nodes = hasher.get_nodes("key2".to_string(), Some(3), Some(10_f64));
+        let nodes = hasher.distribute("key2".to_string(), Some(3), Some(10_f64));
+        println!("node6 added key2 {nodes:?} \n");
         assert_eq!(nodes.len(), 3);
 
         hasher.remove("node6".to_string());
         hasher.remove("node5".to_string());
         hasher.remove("node4".to_string());
         hasher.remove("node3".to_string());
-        let nodes = hasher.get_nodes("key3".to_string(), Some(3), Some(10_f64));
-        assert_eq!(nodes.len(), 1);
+        let nodes = hasher.distribute("key3".to_string(), Some(3), Some(1_f64));
+        println!("node[3-6] removed key3 {nodes:?} \n");
+        assert_eq!(nodes.len(), 2);
 
-        let nodes = hasher.get_nodes("key4".to_string(), Some(3), Some(15_f64));
+        let nodes = hasher.distribute("key4".to_string(), Some(3), Some(50_f64));
+        println!("use too many resources key4 {nodes:?} \n");
         assert_eq!(nodes.len(), 0);
 
         hasher.remove("node2".to_string());
-        let nodes = hasher.get_nodes("key2".to_string(), Some(3), None);
+        let nodes = hasher.distribute("key3".to_string(), Some(3), None);
+        println!("node2 removed, lookup key3 {nodes:?} \n");
         assert_eq!(nodes.len(), 1);
 
         hasher.remove("node1".to_string());
-        let nodes = hasher.get_nodes("key5".to_string(), Some(3), Some(5_f64));
+        let nodes = hasher.distribute("key3".to_string(), Some(3), Some(5_f64));
+        println!("node1 removed key3 {nodes:?}");
         assert_eq!(nodes.len(), 0);
     }
 }
